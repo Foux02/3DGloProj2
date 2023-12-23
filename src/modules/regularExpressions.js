d84beed51daac4ef2757e1d1cf2e9462778c3218
Jane Lutak <foux02911@gmail.com>
2023-12-24 00:37:21 +0300

    Выполнено обычное и усложненное №2

--- src/modules/regularExpressions.js
+++ src/modules/regularExpressions.js
@@ -1,5 +1,6 @@
 const regularExpressions = () => {
   const formNames = document.querySelectorAll('.form-name');
+  const formNamesCss = document.getElementsByClassName('form-name');
   const formPhones = document.querySelectorAll('.form-phone');
   const formEmailes = document.querySelectorAll('.form-email');
   const mess = document.querySelector('.mess');
@@ -12,59 +13,61 @@ const regularExpressions = () => {
         e.target.value = item.value;
         console.log('число');
       } else {
-        alert('Нужно ввести целое число');
         e.target.value = e.target.value.replace(/[^\d]/g, '');
       }
     });
   });
 
-  formNames.forEach((item) => {
+  formNames.forEach((item, index) => {
     item.addEventListener('blur', (e) => {
-      if (/^[а-яА-Я-\s]{0,}$/g.test(item.value)) {
-        e.target.value = item.value;
-      } else {
-        e.target.value = e.target.value.replace(/[\w\D]/g, '');
-      }
+      formNamesCss[index].style.textTransform = 'capitalize';
+      e.target.value = e.target.value.replace(
+        /[a-zA-Z0-9\&\@\/\#\,\+\$\~\%\.\'\"\:\;\*\?\<\>\{\}\!\_\(\)]/g,
+        ''
+      );
+      e.target.value = e.target.value.replace(/[\-]{2,}/g, '-');
+      e.target.value = e.target.value.replace(/[\s]{2,}/g, ' ');
+      e.target.value = e.target.value.replace(/^[\-\s]*/g, '');
+      e.target.value = e.target.value.replace(/[\-\s]*$/g, '');
+
+      console.log('else' + index);
     });
   });
 
   mess.addEventListener('blur', (e) => {
-    if (/^[а-яА-Я-\s]{0,}$/g.test(mess.value)) {
-      e.target.value = mess.value;
-    } else {
-      e.target.value = e.target.value.replace(/[\w\D]/g, '');
-    }
+    mess.style.textTransform = 'capitalize';
+    e.target.value = e.target.value.replace(
+      /[a-zA-Z0-9\&\/\#\,\+\$\~\%\.\'\"\:\;\*\?\<\>\{\}\!\_\(\)]/g,
+      ''
+    );
+    e.target.value = e.target.value.replace(/[\-]{2,}/g, '-');
+    e.target.value = e.target.value.replace(/[\s]{2,}/g, ' ');
+    e.target.value = e.target.value.replace(/^[\-\s]*/g, '');
+    e.target.value = e.target.value.replace(/[\-\s]*$/g, '');
+    mess.style.textTransform = 'capitalize';
   });
 
   formEmailes.forEach((item) => {
     item.addEventListener('blur', (e) => {
-      if (/^[\w\.\-\_\!\~\*\'\@]$/g.test(item.value)) {
-        e.target.value = item.value;
-      } else {
-        e.target.value = e.target.value.replace(
-          /[\[\]\&\/\#\,\+\$\%\"\:\?\<\>\{\}а-яА-Я\s]/g,
-          ''
-        );
-        e.target.value = e.target.value.replace(/[\-]{2,}/g, '-');
-        e.target.value = e.target.value.replace(/^[\-]/g, '');
-        e.target.value = e.target.value.replace(/[\-]$/g, '');
-      }
+      e.target.value = e.target.value.replace(
+        /[\[\]\&\/\#\,\+\$\%\"\:\?\<\>\{\}а-яА-Я\s]/g,
+        ''
+      );
+      e.target.value = e.target.value.replace(/[\-]{2,}/g, '-');
+      e.target.value = e.target.value.replace(/^[\-]*/g, '');
+      e.target.value = e.target.value.replace(/[\-]*$/g, '');
     });
   });
 
   formPhones.forEach((item) => {
     item.addEventListener('blur', (e) => {
-      if (/^[\d\(\)\-]$/g.test(item.value)) {
-        e.target.value = item.value;
-      } else {
-        e.target.value = e.target.value.replace(
-          /[\&\/\#\,\+\$\~\%\.\'\"\:\*\?\<\>\{\}\!\_a-zA-Zа-яА-Я\s]/g,
-          ''
-        );
-        e.target.value = e.target.value.replace(/[\-]{2,}/g, '-');
-        e.target.value = e.target.value.replace(/^[\-]/g, '');
-        e.target.value = e.target.value.replace(/[\-]$/g, '');
-      }
+      e.target.value = e.target.value.replace(
+        /[\&\/\#\,\+\$\~\%\.\'\"\:\*\?\<\>\{\}\!\_a-zA-Zа-яА-Я\s]/g,
+        ''
+      );
+      e.target.value = e.target.value.replace(/[\-]{2,}/g, '-');
+      e.target.value = e.target.value.replace(/^[\-]*/g, '');
+      e.target.value = e.target.value.replace(/[\-]*$/g, '');
     });
   });
 };
