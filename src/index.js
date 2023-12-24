066ce2524d63d3a026b028c01f1db76796ea9bdc
Jane Lutak <foux02911@gmail.com>
2023-12-24 16:39:42 +0300

    Обязательное задание готово

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import regularExpressions from './modules/regularExpressions';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 timer('21 december 2023');
 menu();
 modal();
 regularExpressions();
 tabs();
+slider();
