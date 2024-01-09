a02a5500efcdb68a34643d4d86143338b601cfbd
Jane Lutak <foux02911@gmail.com>
2024-01-10 02:24:20 +0300

    Задание выполнено, но осталось два вопроса

--- lesson26/script2.js
+++ lesson26/script2.js
@@ -1,3 +1,43 @@
-const user = fetch('db.json');
+'use strict';
 
-console.log(user);
+const sendData = (url, data) => {
+  return fetch(url, {
+    method: 'POST',
+    body: data,
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error('Неудачный запрос');
+    }
+    response.json();
+  });
+};
+
+const getData = (url) => {
+  return fetch(url).then((response) => response.json());
+};
+
+try {
+  getData('db.json')
+    .then((data) => {
+      console.log('Данные получены: ', data);
+      console.log(`Данные отправки (вариант 2): ${data}`);
+      sendData(
+        'https://jsonplaceholder.typicode.com/posts',
+        JSON.stringify(data)
+      )
+        .then((data) => {
+          console.log('Данные отправлены: ', data);
+        })
+        .catch((error) => {
+          console.log(`Ошибка отправки: ${error.message}`);
+        });
+    })
+    .catch((error) => {
+      console.log(`Ошибка получения: ${error}`);
+    });
+} catch (error) {
+  console.log(`Ошибка отправки: ${error.message}`);
+}
